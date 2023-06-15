888ca980afdd4c6066a631b7eb4499c2f971ace5
Rodolfo Augusto <rar2023@icloud.com>
2023-06-15 06:54:15 -0300

    nav and footer

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import Image from "next/image";
-import { CustomButtonProps } from "./types";
+import { CustomButtonProps } from "../types";
 
-const Button = ({title, containerStyles, handleClick} : CustomButtonProps) => {
+const Button = ({title, containerStyles, handleClick, btnType} : CustomButtonProps) => {
   return (
     <button
         disabled={false}
-        type={"button"}
+        type={btnType || "button"}
         className={`custom-btn ${containerStyles}`}
-        onClick={() => {}}
+        onClick={handleClick}
     >
         <span className={`flex-1`}>
             {title}
